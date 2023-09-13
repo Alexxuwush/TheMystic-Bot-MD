890b084529ae89c8d60ad619ceea5c4d5cb8c60d
Alexxuwush <143191747+Alexxuwush@users.noreply.github.com>
2023-09-13 07:59:41 -0500

    Update menu-labiblia.js

--- plugins/menu-labiblia.js
+++ plugins/menu-labiblia.js
@@ -19,47 +19,47 @@ if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `*[❗] Los comandos +1
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
-║    ◉— *𝐓𝐇𝐄 𝐒𝐇𝐀𝐃𝐎𝐖 - 𝐁𝐎𝐓* —◉
-║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
-║➤ *𝗛ola, ${taguser}*
-╰═══╡✯✯✯✯✯✯✯✯✯╞═══╯
+    const str = `┏━━━━━━━━━━━━━━━━┓
+    ◉— *𝐓𝐇𝐄 𝐔𝐍𝐊𝐍𝐎𝐖𝐍 - 𝐁𝐎𝐓* —◉
+
+➤ *𝗛ola, ${taguser}*
+┗━━━━━━━━━━━━━━━━┛
 
 ┏━━━━━━━━━━━━━━━━┓
-┃ *< MENU+18 />*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
-┣ ඬ⃟ 🔞 _${usedPrefix}pack_
-┣ ඬ⃟ 🔞 _${usedPrefix}pack2_
-┣ ඬ⃟ 🔞 _${usedPrefix}pack3_
-┣ ඬ⃟ 🔞 _${usedPrefix}videoxxx_
-┣ ඬ⃟ 🔞 _${usedPrefix}videolesbixxx_
-┣ ඬ⃟ 🔞 _${usedPrefix}tetas_
-┣ ඬ⃟ 🔞 _${usedPrefix}booty_
-┣ ඬ⃟ 🔞 _${usedPrefix}ecchi_
-┣ ඬ⃟ 🔞 _${usedPrefix}furro_
-┣ ඬ⃟ 🔞 _${usedPrefix}imagenlesbians_
-┣ ඬ⃟ 🔞 _${usedPrefix}panties_
-┣ ඬ⃟ 🔞 _${usedPrefix}pene_
-┣ ඬ⃟ 🔞 _${usedPrefix}porno_
-┣ ඬ⃟ 🔞 _${usedPrefix}randomxxx_
-┣ ඬ⃟ 🔞 _${usedPrefix}pechos_
-┣ ඬ⃟ 🔞 _${usedPrefix}yaoi_
-┣ ඬ⃟ 🔞 _${usedPrefix}yaoi2_
-┣ ඬ⃟ 🔞 _${usedPrefix}yuri_
-┣ ඬ⃟ 🔞 _${usedPrefix}yuri2_
-┣ ඬ⃟ 🔞 _${usedPrefix}trapito_
-┣ ඬ⃟ 🔞 _${usedPrefix}hentai_
-┣ ඬ⃟ 🔞 _${usedPrefix}nsfwloli_
-┣ ඬ⃟ 🔞 _${usedPrefix}nsfworgy_
-┣ ඬ⃟ 🔞 _${usedPrefix}nsfwfoot_
-┣ ඬ⃟ 🔞 _${usedPrefix}nsfwass_
-┣ ඬ⃟ 🔞 _${usedPrefix}nsfwbdsm_
-┣ ඬ⃟ 🔞 _${usedPrefix}nsfwcum_
-┣ ඬ⃟ 🔞 _${usedPrefix}nsfwero_
-┣ ඬ⃟ 🔞 _${usedPrefix}nsfwfemdom_
-┣ ඬ⃟ 🔞 _${usedPrefix}nsfwglass_
-┣ ඬ⃟ 🔞 _${usedPrefix}hentaipdf *<texto>*_
-┣ ඬ⃟ 🔞 _${usedPrefix}hentaisearch *<texto>*_
+  *< MENU+18 />*
+
+ 🔞 _${usedPrefix}pack_
+ 🔞 _${usedPrefix}pack2_
+ 🔞 _${usedPrefix}pack3_
+ 🔞 _${usedPrefix}videoxxx_
+ 🔞 _${usedPrefix}videolesbixxx_
+ 🔞 _${usedPrefix}tetas_
+ 🔞 _${usedPrefix}booty_
+ 🔞 _${usedPrefix}ecchi_
+ 🔞 _${usedPrefix}furro_
+ 🔞 _${usedPrefix}imagenlesbians_
+ 🔞 _${usedPrefix}panties_
+ 🔞 _${usedPrefix}pene_
+ 🔞 _${usedPrefix}porno_
+ 🔞 _${usedPrefix}randomxxx_
+ 🔞 _${usedPrefix}pechos_
+ 🔞 _${usedPrefix}yaoi_
+ 🔞 _${usedPrefix}yaoi2_
+ 🔞 _${usedPrefix}yuri_
+ 🔞 _${usedPrefix}yuri2_
+ 🔞 _${usedPrefix}trapito_
+ 🔞 _${usedPrefix}hentai_
+ 🔞 _${usedPrefix}nsfwloli_
+ 🔞 _${usedPrefix}nsfworgy_
+ 🔞 _${usedPrefix}nsfwfoot_
+ 🔞 _${usedPrefix}nsfwass_
+ 🔞 _${usedPrefix}nsfwbdsm_
+ 🔞 _${usedPrefix}nsfwcum_
+ 🔞 _${usedPrefix}nsfwero_
+ 🔞 _${usedPrefix}nsfwfemdom_
+ 🔞 _${usedPrefix}nsfwglass_
+ 🔞 _${usedPrefix}hentaipdf *<texto>*_
+ 🔞 _${usedPrefix}hentaisearch *<texto>*_
 ┗━━━━━━━━━━━━━━━━┛`.trim();
     if (m.isGroup) {
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
